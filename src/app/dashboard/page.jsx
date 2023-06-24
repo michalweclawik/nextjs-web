43863e66c5548bb8b7369dcca903d36d22cb9154
michal <michalwagh@gmail.com>
2023-06-24 11:23:39 +0100

    mongo db intergation

--- src/app/dashboard/page.jsx
+++ src/app/dashboard/page.jsx
@@ -1,7 +1,14 @@
 import React from "react";
 import styles from "./page.module.css";
+import { useSWR } from "swr";
 
 const Dashboard = () => {
+  const fetcher = (...args) => fetch(...args).then((res) => res.json());
+  const { data, error, isLoading } = useSWR(
+    "https://jsonplaceholder.typicode.com/posts",
+    fetcher
+  );
+  console.log(error.message);
   return <div className={styles.container}>Dashboard</div>;
 };
 
